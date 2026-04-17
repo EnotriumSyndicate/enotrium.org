2cd2298461a7313e47b49f65acd4d6f11b48c539
Aidistides <208755803+Aidistides@users.noreply.github.com>
2026-04-17 15:35:51 -0400

    Add white logo file and update Navbar to use it

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -233,7 +233,7 @@ export function Navbar({
             <Link href="/" target="_self" className="z-10" aria-label="Enotrium home">
               <div className="flex flex-row items-center">
                 <Image
-                  src={megaOpen && lightMegaMenu ? "/logo.png" : "/logo.jpg"}
+                  src={megaOpen && lightMegaMenu ? "/logo-white.png" : "/logo.jpg"}
                   alt=""
                   width={120}
                   height={32}
